cdc704a8fa78f6465782bed8b25d90bb96ed8225
BMCHACKS <30594382+BMCHACKS@users.noreply.github.com>
2025-03-23 06:27:01 +0500

    Update Question_3.py
    Fixed syntax in regards to marking scheme. 
    
    - Try & Except statements must ONLY be used for opening and closing files and showing necessary errors.

--- files/2024/9618_w24_qp_42/Question_3.py
+++ files/2024/9618_w24_qp_42/Question_3.py
@@ -1,39 +1,40 @@
 
 def ReadData():
+    Temp_Array = []
+    Final_Array = [] 
     try:
-        file = open("HighScoreTable.txt", 'r')
-        index = 0
-        for line in file:
-            player_id = line.rstrip()
-            game_lvl = file.readline().rstrip()
-            score = file.readline().rstrip()
-            HighScores[index] = [player_id, game_lvl, score]
-            index += 1        
+        Temp_Array = open("HighScoreTable.txt", "r").read().split('\n')    
     except IOError:
         print("[ERROR] File Not Found!")
+        
+    for x in range(0, len(Temp_Array), 3):
+        Final_Array.append([Temp_Array[x], Temp_Array[x+1], Temp_Array[x+2]])
+    
+    return Final_Array
 
 
 def OutputHighScores(array):
-    for x in range(len(array)): print(f"{array[x][0]} reached level {array[x][1]} with a score of {array[x][2]}")
+    for x in range(len(array)):
+        print(f"{array[x][0]} reached level {array[x][1]} with a score of {array[x][2]}")
 
 
-def SortSorces(array):
+def SortScores(array):
     for x in range(len(array)):
         swap = False
         for y in range(len(array)-1-x):
-            if array[y][2] < array[y+1][2]: array[y], array[y+1] = array[y+1], array[y]
-            swap = True
+            if (array[y][1] == array[y+1][1] and array[y][2] < array[y+1][2]) or array[y][1] < array[y+1][1]:
+                array[y], array[y+1] = array[y+1], array[y]
+                swap = True
         if not swap: break
     return array
 
 
 # main
-
 HighScores = [["", "", ""] for _ in range(7)]
 
-ReadData()
+HighScores = ReadData()
 print("Before")
 OutputHighScores(HighScores)
-SortSorces(HighScores)
+HighScores = SortScores(HighScores)
 print("After")
 OutputHighScores(HighScores)
