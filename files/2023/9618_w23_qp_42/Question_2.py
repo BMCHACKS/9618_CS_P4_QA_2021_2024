eb685956a73db3879d0233c4078fbe51645ed98b
BMCHACKS <30594382+BMCHACKS@users.noreply.github.com>
2025-03-23 19:41:54 +0500

    Update Question_2.py
    Added Missing part of this question!

--- files/2023/9618_w23_qp_42/Question_2.py
+++ files/2023/9618_w23_qp_42/Question_2.py
@@ -1,4 +1,4 @@
-def IterativeCalculate(Number : int) -> int:
+def IterativeCalculate(Number):
     ToFind = Number
     Total = 0
     while Number != 0:
@@ -8,12 +8,17 @@ def IterativeCalculate(Number : int) -> int:
     return Total
 
 
+def RecursiveValue(Number, ToFind):
+    if Number == 0:
+        return 0
+    else:
+        if ToFind % Number == 0:
+            return Number + RecursiveValue(Number - 1, ToFind)
+        else:
+            return RecursiveValue(Number - 1, ToFind)
+
 value = IterativeCalculate(10)
 print(value)
 
-value = IterativeCalculate(4)
+value = RecursiveValue(50, 50)
 print(value)
-value = IterativeCalculate(100)
-print(value)
-
-
