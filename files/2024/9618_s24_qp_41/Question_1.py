5d0028c89ca8dee592e2e6919f983c85ce9c0f4e
BMCHACKS <30594382+BMCHACKS@users.noreply.github.com>
2025-04-04 03:16:40 +0500

    Update Question_1.py

--- files/2024/9618_s24_qp_41/Question_1.py
+++ files/2024/9618_s24_qp_41/Question_1.py
@@ -1,35 +1,45 @@
-
 def Initialise():
-    numbers = int(input("Enter the quantity of numbers to enter: "))
-    for _ in range(numbers): 
-        while True:
-            n = int(input("Enter a number(1-20): "))
-            if n >= 1 and n <= 20: break
-            print("Number must be between 1 - 20 inclusive!")
-        DataStored.append(n)
+    global NumberItems
+
+    qty = int(input("Enter the quantity of numbers to input (1-20): "))
+    while qty < 1 or qty > 20:
+        print("Quantity must be from 1-20 only!")
+        qty = int(input("Enter the quantity of numbers to input (1-20): "))
+
+    for _ in range(qty):
+        data = int(input("Enter integer to input: "))
+        DataStored.append(data)
+        NumberItems += 1
+
 
 def BubbleSort():
-    for x in range(len(DataStored)):
+    for x in range(NumberItems):
         swap = False
-        for y in range(len(DataStored)-1-x):
-            if DataStored[y] > DataStored[y+1]: DataStored[y], DataStored[y+1], swap = DataStored[y+1], DataStored[y], True 
-        if not swap: break
+        for y in range(NumberItems-1-x):
+            if DataStored[y] > DataStored[y+1]:
+                swap = True
+                DataStored[y], DataStored[y+1] = DataStored[y+1], DataStored[y]
+        if not swap:
+            break
+
 
 def BinarySearch(DataToFind):
-    low, top = 0, len(DataStored)-1
+    low, top = 0, NumberItems
     while low <= top:
-        mid = (low+top) // 2
-        if DataStored[mid] == DataToFind: return mid
-        low = mid+1 if DataToFind > DataStored[mid] else low
-        top = mid-1 if DataToFind < DataStored[mid] else top
-    return -1 
-
+        mid = (low + top) // 2
+        if DataStored[mid] == DataToFind:
+            return mid
+        if DataToFind > DataStored[mid]:
+            low = mid + 1
+        else:
+            top = mid - 1
+    return -1
 
 # main
 
-global NumberItems
-DataStored = []
+DataStored = [] # INTEGER
 NumberItems = 0
+
 
 Initialise()
 print(DataStored)
@@ -37,7 +47,6 @@ print(DataStored)
 BubbleSort()
 print(DataStored)
 
-n = int(input("Enter a number to search for: "))
-pos = BinarySearch(n)
-print(pos)
-
+to_search = int(input("Enter an integer value to search for: "))
+response = BinarySearch(to_search)
+print(response)
